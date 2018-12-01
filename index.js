57b1b2e631cecde8ed64b7b85b89e254dee0a770
Jess Telford <jess+github@jes.st>
2018-12-01 18:40:58 +1100

    Send properly formatted json

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = (req, res) => { res.end('Hello world') };
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ random: Math.random() }));
+};
